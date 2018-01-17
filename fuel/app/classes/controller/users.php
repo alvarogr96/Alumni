dcbffdfa53030573764218f70e7cb96e13ea4aba
alvarogr96 <alvaro_gomez_apps1m1617@cev.com>
2018-01-17 13:46:22 -0500

    register

--- fuel/app/classes/controller/users.php
+++ fuel/app/classes/controller/users.php
@@ -1,29 +1,42 @@
 <?php 
+
+use Firebase\JWT\JWT;
+
 class Controller_Users extends Controller_Rest
 {
     public function post_create()
     {
+   // private $key = "9adsfssads9sa97ass7as97as7d9";
+
         try {
-            if ( ! isset($_POST['name']) && ! isset($_POST['password'])) 
+            if ( empty($_POST['nombre']) || empty($_POST['email']) || empty($_POST['password']) ) 
             {
                 $json = $this->response(array(
                     'code' => 400,
-                    'message' =>  $_POST['name']
+                    'message' =>  'Falta algun campo'
                 ));
                 return $json;
             }
             
            $input = $_POST;
             $user = new Model_Users();
-            $user->name = $input['name'];
+            $user->nombre = $input['nombre'];
+            $user->email = $input['email'];
             $user->password = $input['password'];
+            $user->foto_perfil = 'alvaroiocld';
+            $user->id_rol = 2;
+            $user->id_lista = 1;
             $user->save();
             $json = $this->response(array(
                 'code' => 200,
                 'message' => 'usuario creado',
-                'data' => $input['name']
+                'data' => $user
             ));
             return $json;
+
+
+
+
         } 
         catch (Exception $e) 
         {
@@ -33,14 +46,54 @@ class Controller_Users extends Controller_Rest
             ));
             return $json;
         }
+
         
+    }
+    public function post_recoverPass()
+    {
+        if ( empty($_POST['email'])) 
+            {
+                $json = $this->response(array(
+                    'code' => 400,
+                    'message' =>  'Falta algun campo'
+                ));
+                return $json;
+            }
+    }
+
+    public function post_delete()
+    {
+        $user = Model_Users::find($_POST['id']);
+        $userName = $user->nombre;
+        $user->delete();
+        $json = $this->response(array(
+            'code' => 200,
+            'message' => 'usuario borrado',
+            'data' => $userName
+        ));
+        return $json;
+    }
+
+     public function get_usuarios()
+    {
+        $users = Model_Users::find('all');
+        return $this->response(Arr::reindex($users));
     }
  
     public function get_login()
     {
+        if ( empty($_POST['nombre']) || empty($_POST['password']) ) 
+            {
+                $json = $this->response(array(
+                    'code' => 400,
+                    'message' =>  'Falta algun campo'
+                ));
+                return $json;
+            }
+
         $users = Model_Users::find('all', array(
             'where' => array(
-                array('name', $_POST['name']),
+                array('nombre', $_POST['nombre']),
                 array('password', $_POST['password']),
             )
         ));
--- fuel/app/logs/2018/01/17.php
+++ fuel/app/logs/2018/01/17.php
@@ -19,3 +19,69 @@ WARNING - 2018-01-17 12:36:00 --> Fuel\Core\Fuel::init - The configured locale e
 WARNING - 2018-01-17 13:42:00 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
 WARNING - 2018-01-17 13:42:36 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
 ERROR - 2018-01-17 13:42:36 --> Error - Class 'Model_Users' not found in /var/www/html/Alumni/fuel/app/classes/controller/users.php on line 17
+WARNING - 2018-01-17 17:29:04 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+ERROR - 2018-01-17 17:29:04 --> Error - Class 'Model_Usuarios' not found in /var/www/html/Alumni/fuel/app/classes/controller/users.php on line 20
+WARNING - 2018-01-17 17:35:02 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2018-01-17 17:35:47 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2018-01-17 17:35:52 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2018-01-17 17:36:50 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2018-01-17 17:37:13 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2018-01-17 17:38:17 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2018-01-17 17:38:43 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+ERROR - 2018-01-17 17:38:43 --> Error - Class 'Model_Usuarios' not found in /var/www/html/Alumni/fuel/app/classes/controller/users.php on line 22
+WARNING - 2018-01-17 17:39:18 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2018-01-17 17:40:12 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2018-01-17 17:41:05 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2018-01-17 17:41:18 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2018-01-17 17:45:18 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2018-01-17 17:45:53 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2018-01-17 17:46:18 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2018-01-17 17:48:49 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+ERROR - 2018-01-17 17:48:49 --> Error - syntax error, unexpected 'private' (T_PRIVATE) in /var/www/html/Alumni/fuel/app/classes/controller/users.php on line 9
+WARNING - 2018-01-17 17:51:22 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+ERROR - 2018-01-17 17:51:22 --> Error - Class 'Model_Usuarios' not found in /var/www/html/Alumni/fuel/app/classes/controller/users.php on line 46
+WARNING - 2018-01-17 17:51:44 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2018-01-17 17:51:50 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2018-01-17 17:58:38 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2018-01-17 18:07:03 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+ERROR - 2018-01-17 18:07:03 --> Error - syntax error, unexpected ''password'' (T_CONSTANT_ENCAPSED_STRING), expecting ')' in /var/www/html/Alumni/fuel/app/classes/model/users.php on line 14
+WARNING - 2018-01-17 18:07:38 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+ERROR - 2018-01-17 18:07:38 --> Error - syntax error, unexpected ''email'' (T_CONSTANT_ENCAPSED_STRING), expecting ')' in /var/www/html/Alumni/fuel/app/classes/model/users.php on line 14
+WARNING - 2018-01-17 18:09:30 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+ERROR - 2018-01-17 18:09:30 --> Error - syntax error, unexpected ''email'' (T_CONSTANT_ENCAPSED_STRING), expecting ')' in /var/www/html/Alumni/fuel/app/classes/model/users.php on line 14
+WARNING - 2018-01-17 18:09:58 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+ERROR - 2018-01-17 18:09:58 --> Error - syntax error, unexpected ''id_rol'' (T_CONSTANT_ENCAPSED_STRING), expecting ')' in /var/www/html/Alumni/fuel/app/classes/model/users.php on line 20
+WARNING - 2018-01-17 18:10:15 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2018-01-17 18:11:00 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2018-01-17 18:11:48 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2018-01-17 18:13:18 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2018-01-17 18:14:25 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2018-01-17 18:14:49 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2018-01-17 18:15:11 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2018-01-17 18:16:28 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2018-01-17 18:17:26 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2018-01-17 18:18:06 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2018-01-17 18:18:15 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2018-01-17 18:18:20 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2018-01-17 18:18:49 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2018-01-17 18:19:12 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2018-01-17 18:19:17 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2018-01-17 18:21:42 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2018-01-17 18:21:51 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2018-01-17 18:23:07 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2018-01-17 18:23:19 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2018-01-17 18:24:51 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+ERROR - 2018-01-17 18:24:51 --> Notice - Undefined index: id in /var/www/html/Alumni/fuel/app/classes/controller/users.php on line 49
+WARNING - 2018-01-17 18:25:18 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+ERROR - 2018-01-17 18:25:18 --> Notice - Trying to get property of non-object in /var/www/html/Alumni/fuel/app/classes/controller/users.php on line 50
+WARNING - 2018-01-17 18:26:54 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+ERROR - 2018-01-17 18:26:54 --> Notice - Trying to get property of non-object in /var/www/html/Alumni/fuel/app/classes/controller/users.php on line 50
+WARNING - 2018-01-17 18:28:10 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+ERROR - 2018-01-17 18:28:10 --> Notice - Undefined index: id in /var/www/html/Alumni/fuel/app/classes/controller/users.php on line 49
+WARNING - 2018-01-17 18:28:22 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+ERROR - 2018-01-17 18:28:22 --> Notice - Trying to get property of non-object in /var/www/html/Alumni/fuel/app/classes/controller/users.php on line 50
+WARNING - 2018-01-17 18:29:02 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2018-01-17 18:29:09 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2018-01-17 18:29:13 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2018-01-17 18:29:23 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2018-01-17 18:29:28 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
