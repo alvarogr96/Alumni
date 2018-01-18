71de89a165cebcb0b18622fbafe9b95f00d1677c
alvarogr96 <alvaro_gomez_apps1m1617@cev.com>
2018-01-18 03:45:53 -0500

    register

--- fuel/app/config/development/db.php
+++ fuel/app/config/development/db.php
@@ -6,7 +6,7 @@
 return array(
 	'default' => array(
 		'connection'  => array(
-			'dsn'        => 'mysql:host=localhost;dbname=Alumni',
+			'dsn'        => 'mysql:host=localhost;dbname=alumni',
 			'username'   => 'alumnos',
 			'password'   => 'cev',
 		),
