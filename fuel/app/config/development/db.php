69f358f447dbb4a0e75dff72a45c8edb6032bb2f
alvarogr96 <alvaro_gomez_apps1m1617@cev.com>
2018-01-18 04:15:45 -0500

    server

--- fuel/app/config/development/db.php
+++ fuel/app/config/development/db.php
@@ -6,9 +6,18 @@
 return array(
 	'default' => array(
 		'connection'  => array(
+			'dsn'        => 'mysql:host=h2744356.stratoserver.net;dbname=alumni',
+			'username'   => 'alumni',
+			'password'   => 'PN0hVOFh42lmgapw',
+		),
+	),
+);
+/*
+	'default' => array(
+		'connection'  => array(
 			'dsn'        => 'mysql:host=localhost;dbname=alumni',
 			'username'   => 'alumnos',
 			'password'   => 'cev',
 		),
 	),
-);
+	*/
