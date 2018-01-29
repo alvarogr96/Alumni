7f6b5c7f29628a1539026064a01f6b0a4bd061de
adrianmenendez55 <adrian_menedez_apps1m1617@cev.com>
2018-01-29 08:11:45 -0500

    pruebacosas

--- fuel/app/classes/controller/board.php
+++ fuel/app/classes/controller/board.php
@@ -16,21 +16,21 @@ class Controller_Board extends Controller_Rest
 		try
 		{   
             /*hacer metodo validar token para poder usarlo en cada endpoint*/
-            $header = apache_request_headers();
+            /*$header = apache_request_headers();
             try
             {
                 $this->tokenValidate($header);
             }
-            catch
+            catch (Exception $e)
             {
                 $json = $this->response(array(
                     'code' => 500,
                     'message' => $e->getMessage()
                 ));
                 return $json;
-            }
+            }*/
 
-			if ( empty($_POST['title']) || empty($_POST['description']) || empty($_POST['group']) )
+			if ( empty($_POST['title']) || empty($_POST['description']) || empty($_POST['group']) || empty($_POST['localization']) || empty($_POST['link']) || empty($_POST['id_type']) )
             {
                 $json = $this->response(array(
                     'code' => 400,
@@ -43,12 +43,12 @@ class Controller_Board extends Controller_Rest
             {
                 $input = $_POST;
                 $board = new Model_Board();
-                $board->id_type = $input['id_type'];
                 $board->title = $input['title'];
                 $board->description = $input['description'];
                 $board->group = $input['group'];
                 $board->localization = $input['localization'];
                 $board->link = $input['link'];
+                $board->id_type = $input['id_type'];
                 $board->save();
                 $json = $this->response(array(
                     'code' => 200,
---
+++ fuel/app/logs/2018/01/29.php
@@ -0,0 +1,19 @@
+<?php defined('COREPATH') or exit('No direct script access allowed'); ?>
+
+WARNING - 2018-01-29 12:23:34 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+ERROR - 2018-01-29 12:23:34 --> Error - syntax error, unexpected '{', expecting '(' in /var/www/html/Alumni/fuel/app/classes/controller/board.php on line 25
+WARNING - 2018-01-29 12:24:59 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2018-01-29 12:25:16 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2018-01-29 12:25:29 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2018-01-29 12:25:45 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2018-01-29 12:27:13 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2018-01-29 12:27:50 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2018-01-29 12:36:20 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2018-01-29 12:51:09 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2018-01-29 12:52:50 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2018-01-29 12:59:03 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2018-01-29 13:01:59 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2018-01-29 13:02:48 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2018-01-29 13:03:22 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2018-01-29 13:09:35 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2018-01-29 13:10:27 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
